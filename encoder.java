bfc0e7f606d4e88d23fb2a8cf39ffaf986dcf39b
woofasaurus <dinocove@gmail.com>
2020-09-02 13:10:32 -0700

    Update encoder.java

--- encoder.java
+++ encoder.java
@@ -3,8 +3,10 @@ import java.io.*;
 import java.util.HashMap;
 public class Encoder {
 	private HashMap<String,Integer> hm;
+	private Integer counter;
 	public Encoder() {
 		hm = new HashMap<String, Integer>
+		counter = 0;
 	}
 	public void encode(String filename) {
 		BufferedReader br = new BufferedReader(new FileReader(filename));
@@ -16,9 +18,11 @@ public class Encoder {
 		return hm.containsKey(s);
 	}
 
-	public int addKey (String str)
+	public Integer addKey (String str) //adds new string to HashMap with the value of counter. Increments counter. Returns the associated value of str - 1 letter.
 	{
-		String tempString = str.substring(0,str.length())
+		hm.put(str, counter);
+		counter+;
+		return hm.get(str.substring(0,str.length()-1));;
 	}
 
 
