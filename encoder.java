f6a66541b4316dff492853e25a7bb205edf52cac
KellanMcCarthy <Kellanpmccarthy@gmail.com>
2020-09-02 13:15:09 -0700

    Update encoder.java
    fixed the one comment and the name of file

--- encoder.java
+++ encoder.java
@@ -10,7 +10,7 @@ public class Encoder {
 	}
   public void encode(String filename) {
     BufferedReader br = new BufferedReader(new File(filename));
-    BufferedWriter bw = new BufferedWriter(new FileWriter(filename+".lzw"));
+    BufferedWriter bw = new BufferedWriter(new FileWriter(filename.substring(0, filename.length-4)+".lzw"));
 
 	}
 
@@ -23,7 +23,8 @@ public class Encoder {
 		return hm.containsKey(s);
 	}
 
-	public Integer addKey (String str) { //adds new string to HashMap with the value of counter. Increments counter. Returns the associated value of str - 1 letter.
+  //adds new string to HashMap with the value of counter. Increments counter. Returns the associated value of str - 1 letter.
+	public Integer addKey (String str) {
 		hm.put(str, counter);
 		counter++;
 		return hm.get(str.substring(0,str.length()-1));;
