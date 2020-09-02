af978835770808813ff11d15482329f7941cab2d
Eli Friedman <elifriedman317@gmail.com>
2020-09-02 13:09:28 -0700

    Setting up hasmap methods

--- encoder.java
+++ encoder.java
@@ -4,14 +4,19 @@ import java.util.HashMap;
 public class Encoder {
 	private HashMap<String,Integer> hm;
 	public Encoder() {
-		hm = new HashMap<String, Integer>
+		hm = new HashMap<String, Integer>;
 	}
 	public void encode(String filename) {
 		BufferedReader br = new BufferedReader(new FileReader(filename));
 		BufferedWriter bw = new BufferedWriter(new FileReader(filename+".lzw"));
 
 	}
-  
+	
+	private void firstChar (char c) {
+		hm.put(c, 0);
+		counter++;
+	}
+	
 	private boolean isKey (String s) {
 		return hm.containsKey(s);
 	}
