648f2555c5e83c1b5fc10dde6381d0d048a96cea
woofasaurus <dinocove@gmail.com>
2020-09-02 12:56:46 -0700

    Update encoder.java

--- encoder.java
+++ encoder.java
@@ -1,9 +1,15 @@
 public class encoder {
-  public encoder() {
+	public encoder() {
 
-  }
-  public void encode(String filename) {
-    BufferedReader br = new BufferedReader(new FileReader(filename));
-    BufferedWriter bw = new BufferedWriter(new FileReader(filename+".lzw"));
-  }
+	}
+
+	public void encode(String filename) {
+		BufferedReader br = new BufferedReader(new FileReader(filename));
+		BufferedWriter bw = new BufferedWriter(new FileReader(filename+".lzw"));
+	}
+
+	public int addKey (String str)
+	{
+		String tempString = str.substring(0,str.length())
+	}
 }
