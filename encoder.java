46bc51df4c9f2b01cdb58b614a37d4b9d13d874a
Eli Friedman <elifriedman317@gmail.com>
2020-09-02 12:56:25 -0700

    isKey

--- encoder.java
+++ encoder.java
@@ -1,11 +1,21 @@
+import.java.util.*;
+import java.io.*;
+import java.util.HashMap;
 public class Encoder {
-  public Encoder() {
+	private HashMap<String,Integer> hm;
+	public Encoder() {
+		hm = new HashMap<String, Integer>
+	}
+	public void encode(String filename) {
+		BufferedReader br = new BufferedReader(new FileReader(filename));
+		BufferedWriter bw = new BufferedWriter(new FileReader(filename+".lzw"));
 
-  }
-  public void encode(String filename) {
-    BufferedReader br = new BufferedReader(new FileReader(filename));
-    BufferedWriter bw = new BufferedWriter(new FileReader(filename+".lzw"));
+	}
+  
+	private boolean isKey (String s) {
+		return hm.containsKey(s);
+	}
+  
 
 
-  }
 }
