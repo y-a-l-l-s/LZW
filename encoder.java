82fa714c2bb1b6c509322475dfa2be7ff37bcf91
woofasaurus <dinocove@gmail.com>
2020-09-02 13:16:16 -0700

    Update encoder.java
    moved comment

--- encoder.java
+++ encoder.java
@@ -22,8 +22,8 @@ public class Encoder {
 	private boolean isKey (String s) {
 		return dict.containsKey(s);
 	}
-
-	public Integer addKey (String str) { //adds new string to HashMap with the value of counter. Increments counter. Returns the associated value of str - 1 letter.
+	//adds new string to HashMap with the value of counter. Increments counter. Returns the associated value of str - 1 letter.
+	public Integer addKey (String str) { 
 		dict.put(str, counter);
 		counter++;
 		return dict.get(str.substring(0,str.length()-1));;
