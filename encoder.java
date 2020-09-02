24cc9a5b7252c618b0638deb14c4972a9b2ca58f
Eli Friedman <elifriedman317@gmail.com>
2020-09-02 12:47:04 -0700

    Update encoder.java

--- encoder.java
+++ encoder.java
@@ -1,9 +1,11 @@
-public class encoder {
-  public encoder() {
+public class Encoder {
+  public Encoder() {
 
   }
   public void encode(String filename) {
     BufferedReader br = new BufferedReader(new FileReader(filename));
     BufferedWriter bw = new BufferedWriter(new FileReader(filename+".lzw"));
+
+
   }
 }
