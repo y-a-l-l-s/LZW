8afe8adc5319b2eeea7ff0d1f36596478389b675
KellanMcCarthy <Kellanpmccarthy@gmail.com>
2020-09-02 12:44:50 -0700

    Update encoder.java
    added some encoding stuff

--- encoder.java
+++ encoder.java
@@ -1,5 +1,9 @@
 public class encoder {
-  public encoder () {
+  public encoder() {
 
+  }
+  public void encode(String filename) {
+    BufferedReader br = new BufferedReader(new FileReader(filename));
+    BufferedWriter bw = new BufferedWriter(new FileReader(filename+".lzw"));
   }
 }
