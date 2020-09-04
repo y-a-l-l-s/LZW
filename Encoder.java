1ead7783e2ac17e0cc1035d4dffc00f0ba91feb8
Eli Friedman <elifriedman317@gmail.com>
2020-09-04 10:46:33 -0700

    Printing Base256 2 digits

--- Encoder.java
+++ Encoder.java
@@ -2,12 +2,13 @@ import java.util.*;
 import java.io.*;
 import java.util.HashMap;
 public class Encoder {
-	private HashMap<String,Integer> dict;
-	private Integer counter;
-	public Encoder(int chars) {
+	private HashMap <String,Integer> dict;
+	private int counter, maxCodes;
+	public Encoder(int maxCodes) {
 		dict = new HashMap<String, Integer>();
 		counter = 0;
-		setup(chars);
+		this.maxCodes = maxCodes;
+		setup(256);
 	}
 	public void encode (String fileName) throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader(new File(fileName)));
@@ -33,7 +34,6 @@ public class Encoder {
 				}
 			}
 		}
-
 		br.close();
 		bw.flush();
 		bw.close();
@@ -42,8 +42,11 @@ public class Encoder {
 	//writes the Integer onto the output file
 	private void write (Integer num, BufferedWriter writer) {
 		try {
-			String tempString = "" + num;
-			writer.write(tempString);
+			int c1 = (int) (num / 256);
+			writer.write((char)c1);
+			int c2 = (int) (num % 256);
+			writer.write((char)c2);
+			
 		}
 		catch (Exception ex) {
 			ex.printStackTrace();
@@ -64,8 +67,10 @@ public class Encoder {
 
 	//adds new string to HashMap with the value of counter. Increments counter. Returns the associated value of str - 1 letter.
 	private Integer addKey (String str) {
-		dict.put(str, counter);
-		counter++;
+		if (counter < maxCodes) {
+			dict.put(str, counter);
+			counter++;
+		}
 		return dict.get(str.substring(0,str.length()-1));
 	}
 }
--- EncoderTester.java
+++ EncoderTester.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class EncoderTester{
 	public static void main (String [] args) throws IOException
 	{
-		Encoder eli = new Encoder (256);
+		Encoder eli = new Encoder (65536);
 		eli.encode("message.txt");
 	}
 }
